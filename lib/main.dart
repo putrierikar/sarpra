ef91b718356a563ec5b9fc533482d83fa8ee016b
putrierikar <putrierikar902@gmail.com>
2022-05-27 11:28:02 +0700

    buat circleavatar

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: unnecessary_const
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -59,16 +61,8 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: CustomScrollView(
@@ -97,20 +91,29 @@ class _MyHomePageState extends State<MyHomePage> {
             expandedHeight: 150,
             shape: RoundedRectangleBorder(
               borderRadius: BorderRadius.only(
-                bottomLeft: Radius.circular(100),
-                bottomRight: Radius.circular(100),
+                bottomLeft: Radius.circular(50),
+                bottomRight: Radius.circular(50),
               )
             ),
             
           ),
-          Container(
-            child: 
-                CircleAvatar(
-                  radius: 60,
-                  //backgroundImage: AssetImage('images/1234.jpg'),
-                ),
-              
-            
+          SliverList(
+            delegate: SliverChildListDelegate([
+              Container(
+                child: Padding(
+                    padding: EdgeInsets.only(top: 0,),
+                    child: CircleAvatar(
+                      maxRadius: 50,
+                      child: ClipOval(
+                        child: Image.network('https://i.mydramalist.com/j4QWvc.jpg'),
+                      ),
+                    
+                    ),
+                  ),
+                
+                
+              )
+            ]),
           ),
         ],
       ),
