62f7e6a0569186b3ff2588fea1c23467689185db
putrierikar <putrierikar902@gmail.com>
2022-05-29 22:06:03 +0700

    bottom halaman login

--- lib/user/screens/halaman_login.dart
+++ lib/user/screens/halaman_login.dart
@@ -15,6 +15,7 @@ class _HalamanLoginState extends State<HalamanLogin> {
         children: <Widget>[
           buildTop(),
           buildContent(),
+          buildBottom(),
         ],
       ),
     );
@@ -49,6 +50,52 @@ class _HalamanLoginState extends State<HalamanLogin> {
         Text(
           'Laporin ke Sobat Sarpra aja, yuk!',
           style: Theme.of(context).textTheme.bodyText2,
+        ),
+      ],
+    ),
+  );
+
+
+  Widget buildBottom() => Container(
+    padding: EdgeInsets.only(top: 50, left: 50, right: 50),
+    child: Column(
+      children: [
+        TextFormField(
+          decoration: InputDecoration(
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(12),
+            ),
+            hintText: 'Ketuk untuk isi email kamu',
+            hintStyle: Theme.of(context).textTheme.bodyText2,
+          ),
+        ),
+        SizedBox(height: 20,),
+        TextFormField(
+          decoration: InputDecoration(
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(12),
+            ),
+            hintText: 'Ketuk untuk isi password kamu',
+            hintStyle: Theme.of(context).textTheme.bodyText2,
+          ),
+        ),
+        const SizedBox(height: 70,),
+        Container(
+          height: 40,
+          width: double.infinity,
+          child: TextButton(
+            style: TextButton.styleFrom(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(8),
+                ),
+                backgroundColor: Colors.pink,
+              ),
+            onPressed: (){}, 
+            child: Text(
+              'Masuk',
+              style: Theme.of(context).textTheme.button,
+            ),
+          ),
         ),
       ],
     ),
